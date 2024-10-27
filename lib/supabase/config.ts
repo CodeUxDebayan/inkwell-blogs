693f1efe03cbfee6e15e3dc5544b724582975eb7
Debayan mukherjee <120113934+uxRakhal@users.noreply.github.com>
2024-10-27 15:53:40 +0530

    added dynamic rendering to route.ts on auth/callback

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!
+export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
